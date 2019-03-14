6bd9fc7e07fa481068ecaccfa144c154dc808024
Prince Christian Basiga <princebasiga@gmail.com>
2019-03-13 22:25:01 -0700

    Implemented CaptureItem component

---
+++ frontend/src/library/components/CaptureItem.js
@@ -0,0 +1,74 @@
+
+import React, {Component} from 'react';
+import Camera from 'react-native-camera';
+
+
+
+class CaptureItem extends Component{
+
+    constructor(props){
+
+        super(props);
+        this.takePicture.bind(this);
+    }
+
+     takePicture(){
+
+        this.camera.takePictureAsync({
+            base64:true,
+            //Then after they process the picture, will resume it, will
+            //pass in ref's function address to call.
+            pauseAfterCapture: true
+        })
+        .then (picture => {
+
+            this.props.onCapture(this.camera.resumePreview, picture);
+
+        })            
+        .catch (err => {
+
+            console.log("Failed to take picture");
+        })
+
+    }
+
+    render(){
+
+        return (<View style = {styles.container}>
+            <Camera ref  = {this.camera = ref} style ={styles.preview}/>
+            {/*Overlay maybe from capturing item to create hunt or for being in hunt*/}
+            {this.props.overlay}
+            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={this.takePicture} style={styles.capture}>
+                    <Text style={{ fontSize: 14 }}> CAPTURE </Text>
+                </TouchableOpacity>
+             </View>
+
+        </View>);
+    }
+
+}
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+      },
+      preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      },
+
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+      },
+});
+export default CaptureItem;
