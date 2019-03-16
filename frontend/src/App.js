f1b1b3245c0ba715697da45c901a7389784583d3
Prince Christian Basiga <princebasiga@gmail.com>
2019-03-15 17:00:27 -0700

    Moved location of files

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+
+import React, {Component} from 'react';
+import  {createAppContainer, createSwitchNavigator } from 'react-navigation';
+/*
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+*/
+
+import HomeScreen from './screens/home';
+
+export default class App extends Component{
+
+  //Loads in everything required, ie: all of the items.
+  constructor(props){
+
+    super(props);
+
+    //I don't think redux really needed for just these 3.
+    //We'll see.
+    this.state = {
+      hunts:[],
+      cameraPermission:false,
+      mapPermission:false,
+    };
+  }
+
+
+  render(){
+
+    return <AppContainer/>
+
+  }
+
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+
+    Home: {screen:HomeScreen}
+});
+
+const AppContainer = createAppContainer(AppSwitchNavigator)
